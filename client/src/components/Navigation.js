771062d8576095b2993e7cc31583c04f5bf00120
Saad <saadkhaliq15@gmail.com>
2023-05-15 00:06:12 +0500

    Improved Homepage and Navigation Menu with Brand Colours

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './Styles/Navigation.css';
 
 function Navigation() {
   return (
-    <nav>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/login">Login</a></li>
-        <li><a href="/signup">Sign Up</a></li>
+    <nav className="navigation">
+      <ul className="navigation__menu">
+        <li className="navigation__item"><a href="/">Home</a></li>
+        <li className="navigation__item"><a href="/login">Login</a></li>
+        <li className="navigation__item"><a href="/signup">Sign Up</a></li>
       </ul>
     </nav>
   );
