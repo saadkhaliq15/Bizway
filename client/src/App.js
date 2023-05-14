eae2e645c2ada749a650b5a81251123cc88a1277
Saad <saadkhaliq15@gmail.com>
2023-05-14 22:54:35 +0500

    Developed the index.html page and Mounted the React app

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,42 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
-import Dashboard from './components/Dashboard';
+
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp} />
-        <Route path="/dashboard" component={Dashboard} />
-      </Switch>
-    </Router>
+    <div>
+      <Navigation />
+      <h1>Welcome to Bizway</h1>
+      <p>Welcome to the new era of business.</p>
+    </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import HomePage from './components/HomePage';
+// import Login from './components/Login';
+// import SignUp from './components/SignUp';
+// import Dashboard from './components/Dashboard';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Switch>
+//         <Route path="/" exact component={HomePage} />
+//         <Route path="/login" component={Login} />
+//         <Route path="/signup" component={SignUp} />
+//         <Route path="/dashboard" component={Dashboard} />
+//       </Switch>
+//     </Router>
+//   );
+// }
+
+// export default App;
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,1 +1,15 @@
+import React from 'react';
 
+function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/login">Login</a></li>
+        <li><a href="/signup">Sign Up</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
