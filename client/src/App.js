d2c509aa607323b5c01870ffe87a0eddbf39b15a
Saad <saadkhaliq15@gmail.com>
2023-05-14 23:41:47 +0500

    Adding CSS styling and elegant design to Home Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,14 @@
 import React from 'react';
-
 import Navigation from './components/Navigation';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div>
       <Navigation />
-      <h1>Welcome to Bizway</h1>
-      <p>Welcome to the new era of business.</p>
+      <HomePage />
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import HomePage from './components/HomePage';
-// import Login from './components/Login';
-// import SignUp from './components/SignUp';
-// import Dashboard from './components/Dashboard';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Switch>
-//         <Route path="/" exact component={HomePage} />
-//         <Route path="/login" component={Login} />
-//         <Route path="/signup" component={SignUp} />
-//         <Route path="/dashboard" component={Dashboard} />
-//       </Switch>
-//     </Router>
-//   );
-// }
-
-// export default App;
--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './HomePage.css';
 
 function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <div className="home">
+      <h1 className="home__title">Welcome to Bizway</h1>
+      <p className="home__description">Reshaping Businesses with AI-powered Digital Marketing Automation</p>
+      <button className="home__button">Get Started</button>
+    </div>
+  );
 }
 
 export default HomePage;
